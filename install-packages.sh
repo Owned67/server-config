a8ecd9b52c8384ceb7510639a29125186c97dd50
Owned67 <qx9650@hotmail.fr>
2016-01-18 03:07:02 +0100

    Update install-packages.sh

--- install-packages.sh
+++ install-packages.sh
@@ -11,23 +11,15 @@ echo "==========================================================================
 echo "=== Enabling i386 packages...                                                ==="
 echo "================================================================================"
 dpkg --add-architecture i386
-apt-get update --assume-yes
-apt-get upgrade --assume-yes
+apt-get update --assume-yes && apt-get upgrade --assume-yes
 
 echo "================================================================================"
 echo "=== Installing 32-bit libraries...                                           ==="
 echo "================================================================================"
-apt-get install --assume-yes libstdc++6:i386
-apt-get install --assume-yes libc6:i386
-apt-get install --assume-yes lib32z1-dev
-apt-get install --assume-yes lib32z1
+apt-get install --assume-yes build-essential ia32-libs libstdc++6:i386 libc6:i386 lib32z1-dev lib32z1
+#ubuntu user: libncurses5:i386 libstdc++6:i386
 
 echo "================================================================================"
 echo "=== Installing additional packages                                           ==="
 echo "================================================================================"
-apt-get install --assume-yes vim
-apt-get install --assume-yes screen
-apt-get install --assume-yes rsync
-apt-get install --assume-yes git
-apt-get install --assume-yes unzip
-apt-get install --assume-yes mysql-client
+apt-get install --assume-yes screen rsync git unzip
